febf1850e09cf34a395880179b5edcff5e1f7f83
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-12-06 16:24:41 -0800

    refactor: replace snowflake-odbc on the top level with the one from R demo
    The one developed for R demo is a newer version and the package includes
    some fixes

--- packages/snowflake-odbc/package.nix
+++ packages/snowflake-odbc/package.nix
@@ -1,17 +1,20 @@
-{ fetchurl
-, stdenv
+{ stdenv
+, lib
+, unixODBC
+, openssl
+, fetchurl
 ,
 }:
 let
-  version = "3.4.1";
+  version = "3.5.0";
   sources = {
     "x86_64-linux" = {
       url = "https://sfc-repo.snowflakecomputing.com/odbc/linux/${version}/snowflake_linux_x8664_odbc-${version}.tgz";
-      sha256 = "sha256-q4MECeJUews68RWdAufgdt0+hPyB8QZ5GQqhJCgDQJQ=";
+      sha256 = "sha256-PaYX3Bgt4zqU9f1lUjYtDct685Fgk6LTQBi2K27j/lQ=";
     };
     "aarch64-linux" = {
       url = "https://sfc-repo.snowflakecomputing.com/odbc/linuxaarch64/${version}/snowflake_linux_aarch64_odbc-${version}.tgz";
-      sha256 = "sha256-5Iui4KrsP1U/ZtUyWEOKbm0Gyg8B377VMzhLby2LDpY=";
+      sha256 = "sha256-MORf5kY5b6059jsr4egPwBc0c3FXMEI8+p446EqY8fk=";
     };
   };
 in
@@ -24,6 +27,22 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/lib
     cp -r lib/* $out/lib
+    cp -r ErrorMessages/en-US $out/lib
   '';
-}
 
+  postFixup = ''
+    patchelf --set-rpath ${
+      lib.makeLibraryPath [
+        unixODBC
+        openssl
+        stdenv.cc.cc
+      ]
+    } \
+      $out/lib/libSnowflake.so
+  '';
+
+  passthru = {
+    fancyName = "SnowflakeDSIIDriver";
+    driver = "lib/libSnowflake.so";
+  };
+}
